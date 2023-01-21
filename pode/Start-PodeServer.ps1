1e5617c3a2e7bc8281828f4c0c0af2cffefe6ce9
Martin Walther <it@martin-walther.ch>
2023-01-21 14:11:49 +0100

    Update pode

--- pode/Start-PodeServer.ps1
+++ pode/Start-PodeServer.ps1
@@ -14,6 +14,84 @@
 [CmdletBinding()]
 param ()
 
+#region functions
+function Test-IsAdministrator {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [OSType]$OS
+    )
+
+    Write-Verbose $('[', (Get-Date -f 'yyyy-MM-dd HH:mm:ss.fff'), ']', '[ Begin   ]', "$($MyInvocation.MyCommand.Name)" -Join ' ')
+    if($OS -eq [OSType]::Windows){
+        $user = [Security.Principal.WindowsIdentity]::GetCurrent()
+        $ret  = (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+    }elseif($OS -eq [OSType]::Mac){
+        $ret = ((id -u) -eq 0)
+    }
+
+    Write-Verbose $('[', (Get-Date -f 'yyyy-MM-dd HH:mm:ss.fff'), ']', '[ End     ]', "$($MyInvocation.MyCommand.Name)" -Join ' ')
+    return $ret
+}
+
+function Set-HostEntry{
+    [CmdletBinding()]
+    [Parameter(Mandatory=$true)]
+    [String] $Name,
+
+    [Parameter(Mandatory=$true)]
+    [String]$OS
+
+    Write-Verbose $('[', (Get-Date -f 'yyyy-MM-dd HH:mm:ss.fff'), ']', '[ Begin   ]', "$($MyInvocation.MyCommand.Name)" -Join ' ')
+
+    $PsNetHostsTable = Get-PsNetHostsTable
+    if($PsNetHostsTable.ComputerName -contains $Name){
+        $ret = $true
+    }else{
+        $ElevatedPrivileges = $false
+        if(Test-IsAdministrator -OS $OS) {
+            $ElevatedPrivileges = $true
+        }
+        if($ElevatedPrivileges) {
+            Write-Host "Try to add $($Name) to hosts-file" -ForegroundColor Green
+            Add-PsNetHostsEntry -IPAddress 127.0.0.1 -Hostname $($Name) -FullyQualifiedName "$($Name).local"
+        }else{
+            Write-Host "Try to add $($Name) to hosts-file need elevated Privileges" -ForegroundColor Yellow
+            $ret = $false
+        }
+    }
+    Write-Host $(($PsNetHostsTable | Where-Object ComputerName -match $($Name)) | Out-String)
+
+    Write-Verbose $('[', (Get-Date -f 'yyyy-MM-dd HH:mm:ss.fff'), ']', '[ End     ]', "$($MyInvocation.MyCommand.Name)" -Join ' ')
+
+    return $ret
+}
+
+function Set-PodeRoutes {
+    [CmdletBinding()]
+    param()
+
+    Write-Verbose $('[', (Get-Date -f 'yyyy-MM-dd HH:mm:ss.fff'), ']', '[ Begin   ]', "$($MyInvocation.MyCommand.Name)" -Join ' ')
+
+    Add-PodeRoute -Method Get -Path '/' -ScriptBlock {
+        Write-PodeViewResponse -Path 'index.md'
+    }
+    Add-PodeRoute -Method Get -Path '/html' -ScriptBlock {
+        Write-PodeViewResponse -Path 'html/HTML-ESXiHost-Inventory'
+    }
+    Add-PodeRoute -Method Get -Path '/md' -ScriptBlock {
+        Write-PodeViewResponse -Path 'md/Markdown-ESXiHost-Inventory.md'
+    }
+    Add-PodeRoute -Method Get -Path '/pshtml' -ScriptBlock {
+        Write-PodeViewResponse -Path 'pshtml/PSHTML-ESXiHost-Inventory'
+    }
+
+    Write-Verbose $('[', (Get-Date -f 'yyyy-MM-dd HH:mm:ss.fff'), ']', '[ End     ]', "$($MyInvocation.MyCommand.Name)" -Join ' ')
+
+}
+#endregion
+
+#region Main
 enum OSType {
     Linux
     Mac
@@ -27,29 +105,20 @@ if($PSVersionTable.PSVersion.Major -lt 6){
     if($IsLinux)  {$CurrentOS = [OSType]::Linux}
     if($IsWindows){$CurrentOS = [OSType]::Windows}
 }
+#endregion
 
-function Test-IsAdministrator {
-    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
-    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-}
-
+#region Pode server
 if($CurrentOS -eq [OSType]::Windows){
-    if(Test-IsAdministrator) {
+    if(Test-IsAdministrator -OS $CurrentOS) {
+        $null = Set-HostEntry -Name 'pspode' -OS $CurrentOS
         Start-PodeServer {
-            Write-Host "Running on Windows with elevated Privileges" -ForegroundColor Red
+            Write-Host "Running on Windows with elevated Privileges since $(Get-Date)" -ForegroundColor Red
             Write-Host "Press Ctrl. + C to terminate the Pode server" -ForegroundColor Yellow
 
             Add-PodeEndpoint -Address * -Port 5989 -Protocol Http -Hostname 'pspode'
+            New-PodeLoggingMethod -File -Name 'requests' -MaxDays 4 | Enable-PodeRequestLogging
 
-            Add-PodeRoute -Method Get -Path '/' -ScriptBlock {
-                Write-PodeViewResponse -Path 'index.md'
-            }
-            Add-PodeRoute -Method Get -Path '/html' -ScriptBlock {
-                Write-PodeViewResponse -Path 'html/HTML-ESXiHost-Inventory'
-            }
-            Add-PodeRoute -Method Get -Path '/pshtml' -ScriptBlock {
-                Write-PodeViewResponse -Path 'pshtml/PSHTML-ESXiHost-Inventory'
-            }
+            Set-PodeRoutes
             
         } 
     }else{
@@ -62,20 +131,20 @@ if($CurrentOS -eq [OSType]::Windows){
     }
 }elseif($CurrentOS -eq [OSType]::Mac){
     Start-PodeServer {
-        Write-Host "Running on Mac" -ForegroundColor Cyan
+        if(Test-IsAdministrator -OS $CurrentOS) {
+            $IsRoot = 'with elevated Privileges'
+        }else{
+            $IsRoot = 'as User'
+        }
+        $null = Set-HostEntry -Name 'pspode' -OS $CurrentOS
+        Write-Host "Running on Mac $($IsRoot) since $(Get-Date)" -ForegroundColor Cyan
         Write-Host "Press Ctrl. + C to terminate the Pode server" -ForegroundColor Yellow
 
-        Add-PodeEndpoint -Address * -Port 5989 -Protocol Http
+        Add-PodeEndpoint -Address * -Port 5989 -Protocol Http -Hostname 'pspode'
+        New-PodeLoggingMethod -File -Name 'requests' -MaxDays 4 | Enable-PodeRequestLogging
 
-        Add-PodeRoute -Method Get -Path '/' -ScriptBlock {
-            Write-PodeViewResponse -Path 'index.md'
-        }
-        Add-PodeRoute -Method Get -Path '/html' -ScriptBlock {
-            Write-PodeViewResponse -Path 'html/HTML-ESXiHost-Inventory'
-        }
-        Add-PodeRoute -Method Get -Path '/pshtml' -ScriptBlock {
-            Write-PodeViewResponse -Path 'pshtml/PSHTML-ESXiHost-Inventory'
-        }
+        Set-PodeRoutes
     
     }
 }
+#endregion
--- pode/views/index.md
+++ pode/views/index.md
@@ -2,21 +2,21 @@
     <link href="\assets\style\style.css" rel="stylesheet" />
     <style>
         header{
-            margin:10; background: #033b63 !important;  text-align: center; font-family: "QuickSand", sans-serif;
+            height: 16%; margin:10; background: #033b63 !important;  text-align: center; font-family: "QuickSand", sans-serif;
         }
-        body { 
+        body{
             margin:0; background: #212529 !important;  text-align: center; font-family: "QuickSand", sans-serif;
         }
         div {
             text-align: center; color: #e9ecef;
         }
-        article { 
-            padding: 120px; background: #034f84
+        article{
+            padding: 1%; width: 98%; height: 68.5%; background: #034f84;
         }
-        table {
-            padding: 16px; color: #e9ecef; background: #034f84; text-align: left; 
+        table{
+            padding: 16px; color: #e9ecef; background: #034f84; text-align: left;
         }
-        footer {
+        footer{
             color: green; text-align: left
         }
     </style>
@@ -41,6 +41,7 @@ This is an index example page written in markdown.
 Title|Link
 -|-
 Open the default page|[HTML-ESXiHost-Inventory](http://pspode:5989/html)
+Open the Markdown page|[Markdown-ESXiHost-Inventory](http://pspode:5989/md)
 Open the advanced page|[PSHTML-ESXiHost-Inventory](http://pspode:5989/pshtml)
 
 </table></center>
@@ -49,6 +50,4 @@ Open the advanced page|[PSHTML-ESXiHost-Inventory](http://pspode:5989/pshtml)
 
 </body>
 
-<footer>
-I ♥ PS > I'm running on Pode server ...
-</footer>
+<footer>I ♥ PS > I'm running on Pode server ...</footer>
